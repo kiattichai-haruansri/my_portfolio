514ce4c87879d044e79429c67b6305db9ea8bcbe
Kiattichai <gading.5457.00@gmail.com>
2023-04-13 19:31:05 +0700

    a

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Head from 'next/head'
+import logo from '@/img/profile.jpg'
 
 export default function Layout({children}) {
   return (
     <>
     <Head>
       <title>My Portfolio</title>
-      <link rel="icon" type="image/x-icon" href="../.././profile.png"></link>
+      <link rel="icon" type="image/x-icon" href={logo}></link>
     </Head>
     <Navbar/>
     <main>{children}</main>
