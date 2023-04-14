5b4ee709890d79f19ff8114a209eea19a8d9e336
Kiattichai <gading.5457.00@gmail.com>
2023-04-14 20:29:09 +0700

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Head from 'next/head'
 
 export default function Layout({children}) {
   return (
     <>
-    <Head>
-      <title>My Portfolio</title>
-      <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
-    </Head>
     <Navbar/>
     <main>{children}</main>
     </>
